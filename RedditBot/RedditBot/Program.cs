96d2571531d91ed984c6df214071dfaa64f1d95b
Andre.van.den.ecker <andre.van.den.ecker@accenture.com>
2020-01-27 13:04:45 +0100

    Created Solution & Added Path for Authentication

---
+++ RedditBot/RedditBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBot
+{
+    class Program
+    {
+        public static string AuthInfoLocation;
+        static void Main(string[] args)
+        {
+            AuthInfoLocation = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\RedditBot.cfg";
+            Console.ReadLine();
+        }
+    }
+}
